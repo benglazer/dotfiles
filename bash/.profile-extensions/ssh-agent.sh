799b329c01ed73e35507556baffbe4ee5bdf3ea4
Ben Glazer <ben@benglazer.com>
2024-02-25 01:18:07 -0600

    ssh-agent: Several improvements/simplifications

--- bash/.profile-extensions/ssh-agent.sh
+++ bash/.profile-extensions/ssh-agent.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-SSH_ENV="$HOME/.ssh/environment"
+# Modified from https://web.archive.org/web/20220513101246/http://mah.everybody.org/docs/ssh
+
+SSH_ENV="${HOME}/.ssh/ssh-agent-env"
 
 function start_agent {
-     echo "Initialising new SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-     echo succeeded
-     chmod 600 "${SSH_ENV}"
-     source "${SSH_ENV}" > /dev/null
-     /usr/bin/ssh-add;
+    echo -n "Initializing new SSH agent... "
+    /usr/bin/ssh-agent | sed '/^echo/d' > "${SSH_ENV}"
+    echo succeeded
+    chmod 600 "${SSH_ENV}"
+    source "${SSH_ENV}" > /dev/null
+    ssh-add || true
 }
 
 # Source SSH settings, if applicable
 if [[ -f "${SSH_ENV}" ]]; then
-     source "${SSH_ENV}" > /dev/null
-     #ps ${SSH_AGENT_PID} #doesn't work under cywgin
-     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-         start_agent;
-     }
+    source "${SSH_ENV}"
+    pgrep ssh-agent | grep "${SSH_AGENT_PID}" > /dev/null && echo "Using ssh-agent that's already running" || start_agent
 else
-     start_agent;
+    start_agent
 fi
-
