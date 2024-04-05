1b5933d7618a9bff982a22808483cf768211e192
Ben Glazer <ben@benglazer.com>
2024-04-05 16:10:06 -0500

    Add pycharm installer

---
+++ installers/pycharm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if ! command -v pycharm-community &> /dev/null; then
+        echo "pycharm-community could not be found. Attempting to install pyenv..."
+
+        # Pre-set to avoid interactive prompt from tzdata apt package
+        sudo apt install libsquashfuse0 squashfuse fuse snapd
+        sudo snap install pycharm-community --classic
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install pycharm-ce
+else
+    echo "Cannot install PyCharm: Unsupported OS."
+    exit 1
+fi
