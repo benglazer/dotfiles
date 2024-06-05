6c412223684b7d2f1f36a8bee7e11c44c42b5fd7
Ben Glazer <ben@benglazer.com>
2024-06-05 00:26:42 -0500

    pyenv: Initialize pyenv fully

--- bash/.profile-extensions/pyenv.sh
+++ bash/.profile-extensions/pyenv.sh
@@ -1,6 +1,8 @@
 # Enable pyenv
-# Via https://github.com/pyenv/pyenv?tab=readme-ov-file#set-up-your-shell-environment-for-pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 
 # Custom functions
